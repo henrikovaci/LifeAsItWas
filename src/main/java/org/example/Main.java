732bda2eba8773ff08cf26212d95315a1571fcef
henri.kovaci <henri89kovaci@gmail.com>
2026-08-27 16:07:57 +0200

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    static void main() {
+
+        IO.println(String.format("Adorable Justice"));
+        IO.println(String.format("Do not give me the attributes those pieces of shit have"));
+        IO.println(String.format("give me joy and love"));
+
+
+
+    }
+}
